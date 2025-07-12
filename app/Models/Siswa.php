8a47c8857d31cef04c56ee1fb648985e43bf23da
Muhammad Fathurrahman <muhammadfathurrahman013@gmail.com>
2025-07-12 14:58:00 +0700

    membuat factory

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Siswa extends Model
 {
+     use HasFactory;
     protected $fillable = [
         'nama', 'alamat', 'agama', 'jenis_kelamin', 'asal_sekolah'
     ];
